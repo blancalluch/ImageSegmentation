a0e1975104f408204669c6910946330f24e2f4cb
ferrero-felipe <ferrero.felipe@gmail.com>
2020-02-13 17:27:04 +0100

    mejor equipo de TAs

---
+++ imageProcessing.py
@@ -0,0 +1,45 @@
+import torch
+from torchvision import models
+import torchvision.transforms as T
+import numpy as np
+
+fcn = models.segmentation.fcn_resnet101(pretrained=True).eval()
+
+def decode_segmap(image, nc=21):
+    label_colors = np.array([(0, 0, 0),  # 0=background
+               # 1=aeroplane, 2=bicycle, 3=bird, 4=boat, 5=bottle
+               (128, 0, 0), (0, 128, 0), (128, 128, 0), (0, 0, 128), (128, 0, 128),
+               # 6=bus, 7=car, 8=cat, 9=chair, 10=cow
+               (0, 128, 128), (128, 128, 128), (64, 0, 0), (192, 0, 0), (64, 128, 0),
+               # 11=dining table, 12=dog, 13=horse, 14=motorbike, 15=person
+               (192, 128, 0), (64, 0, 128), (192, 0, 128), (64, 128, 128), (192, 128, 128),
+               # 16=potted plant, 17=sheep, 18=sofa, 19=train, 20=tv/monitor
+               (0, 64, 0), (128, 64, 0), (0, 192, 0), (128, 192, 0), (0, 64, 128)])
+
+    r = np.zeros_like(image).astype(np.uint8)
+    g = np.zeros_like(image).astype(np.uint8)
+    b = np.zeros_like(image).astype(np.uint8)
+
+    for l in range(0, nc):
+        idx = image == l
+        r[idx] = label_colors[l, 0]
+        g[idx] = label_colors[l, 1]
+        b[idx] = label_colors[l, 2]
+
+    rgb = np.stack([r, g, b], axis=2)
+    return rgb
+
+def resizeImg(img,dimension=256):
+    t=T.Compose([T.Resize(dimension)])
+    img=t(img)
+    return img
+
+def generateMask(img,net=fcn):
+    trf = T.Compose([T.ToTensor(), 
+                   T.Normalize(mean = [0.485, 0.456, 0.406], 
+                               std = [0.229, 0.224, 0.225])])
+    inp = trf(img).unsqueeze(0)
+    out = net(inp)['out']
+    om = torch.argmax(out.squeeze(), dim=0).detach().cpu().numpy()
+    rgb = decode_segmap(om)
+    return rgb
